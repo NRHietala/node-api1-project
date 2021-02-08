4ac9f6ceb116d4fee9f6197038805b30553e5ba4
Nathan Hietala <Nrhietala@gmail.com>
2021-02-08 16:07:45 -0600

    working on put

--- api/server.js
+++ api/server.js
@@ -1,19 +1,11 @@
 const generate = require('shortid');
 const express = require('express');
+const server = express();
+server.use(express.json());
 
 const dbFunc = require('./users/model');
 
-const users = [
-  {id:generate(), name:"Bob", job: "Clown"},
-  {id:generate(), name:"Tom", job: "Mattress Salesman"},
-  {id:generate(), name:"Steve", job: "Racer"},
-  {id:generate(), name:"Jake", job: "Alcoholic"},
-];
-
-const app = express();
-app.use(express.json());
-
-app.get('/api/users', (req, res) => {
+server.get('/api/users', (req, res) => {
   dbFunc.find().then(users => {
     res.status(200).json(users)
   })
@@ -22,11 +14,49 @@ app.get('/api/users', (req, res) => {
   })
 });
 
-app.get('/api/users/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id
 
+  dbFunc.findById(id)
+  .then(user => res.status(200).json(user))
+  .catch(err => res.status(404).json(err.message))
+});
+
+server.post('/api/users', (req, res) => {
+ 
+  dbFunc.insert(req.body)
+  .then(user => res.status(201).json(user))
+  .catch(err => res.status(400).json(err.message))
+});
+
+server.put('/api/user/:id', async (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+
+  if(!changes.name || !changes.bio) {
+    res.status(400).json({ message:"All fields required!" })
+  } else {
+    try {
+      const updated = await dbFunc.update(id, changes)
+      res.status(200).json(updated)
+    } catch (error) {
+      res.status(500).json({ message:error })
+    }
+  }
 })
 
+server.delete('/api/user/:id', (req, res) => {
+  const id = req.params.id;
+  dbFunc.remove()
+  .then(user => res.status(200).json(user))
+  .catch(err => res.status(500).json(err.message))
+});
 
+module.exports = server;
 
-
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// const users = [
+//   {id:generate(), name:"Bob", job: "Clown"},
+//   {id:generate(), name:"Tom", job: "Mattress Salesman"},
+//   {id:generate(), name:"Steve", job: "Racer"},
+//   {id:generate(), name:"Jake", job: "Alcoholic"},
+// ];
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const server = require('./api/server');
 const PORT = 5555;
-const express = require('express');
-const app = express();
-app.use(express.json());
 
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
